4308523f63d5fe156dbdb6e897b49a6d55aa3d55
Sergey Sizov <harrior@gmail.com>
2023-03-27 18:18:17 +0400

    refactor: update format in admin

--- MoneyDistrib/core/admin.py
+++ MoneyDistrib/core/admin.py
@@ -5,22 +5,62 @@ from .models import CustomUser
 
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'inn', 'balance', 'is_staff', 'is_active',)
-    list_filter = ('is_staff', 'is_active',)
+    list_display = (
+        "username",
+        "email",
+        "inn",
+        "balance",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = (
+        "is_staff",
+        "is_active",
+    )
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'inn', 'balance')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        (None, {"fields": ("username", "password")}),
+        (
+            "Personal info",
+            {"fields": ("first_name", "last_name", "email", "inn", "balance")},
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2', 'inn', 'balance', 'is_staff', 'is_active'),
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "password1",
+                    "password2",
+                    "inn",
+                    "balance",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
     )
-    search_fields = ('username', 'email', 'inn',)
-    ordering = ('username',)
+    search_fields = (
+        "username",
+        "email",
+        "inn",
+    )
+    ordering = ("username",)
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
